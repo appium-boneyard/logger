85047238d09af6e114190209c85945c4108107ba
sebv <seb.vincent@gmail.com>
2015-05-22 11:20:29 -0700

    configures travis

--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,8 @@
 var gulp = require('gulp'),
     boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
-boilerplate({build: 'appium-logger', jscs: false});
+boilerplate({
+  build: 'appium-logger',
+  jscs: false,
+  testReporter: process.env.TRAVIS ? 'spec' : 'nyan'
+});
